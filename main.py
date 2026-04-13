d9763a9f548d6abacea14eec6f223029516f086c
Georgios <geotrianta145@gmail.com>
2026-04-13 13:54:43 +0200

    Remove irrelevant comment from main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# This is a sample Python script.
+# das gehört nicht zu den Aufgaben
 
+# This is a sample Python script.
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
